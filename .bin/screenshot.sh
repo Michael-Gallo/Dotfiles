5decda41d704ba04240aa802913f61909528d161
Michael Gallo <michael.a.gallo95@gmail.com>
2020-03-28 22:00:43 -0400

    better line spacing

--- .bin/screenshot.sh
+++ .bin/screenshot.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-
-filename=$(echo -e $(date +"%T.png")|dmenu -i)
+# a simple shell script to take pictures with imagemagick , takes current time as default image name and saves to ~/pictures/screenshots
+# if there is an argument it'll take a picture of the entire desktop
+# filename=$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i)
 storage_directory="$HOME/pictures/screenshots"
-([ -d $storage_directory ]||mkdir -pv $storage_directory) ; import "$storage_directory/$filename"
+imgmgck_cmd=$([ -z "$1" ] && echo -e "import" || echo -e "import -window root")
+([ -d $storage_directory ]\
+	||mkdir -pv $storage_directory) \
+	; $imgmgck_cmd "$storage_directory/$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i).png"
