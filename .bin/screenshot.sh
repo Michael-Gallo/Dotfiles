85ed5fa31c71c66ce2cc02405878c02e0c61974f
Michael Gallo <michael.a.gallo95@gmail.com>
2020-03-29 15:08:45 -0400

    screenshot.sh now uses scrot

--- .bin/screenshot.sh
+++ .bin/screenshot.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# a simple shell script to take pictures with imagemagick , takes current time as default image name and saves to ~/pictures/screenshots
+# a simple shell script to take pictures with scrot , takes current time as default image name and saves to ~/pictures/screenshots
 # if there is an argument it'll take a picture of the entire desktop
 # filename=$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i)
 storage_directory="$HOME/pictures/screenshots"
-imgmgck_cmd=$([ -z "$1" ] && echo -e "import" || echo -e "import -window root")
+scrot_command=$([ -z "$1" ] && echo -e "scrot -s" || echo -e "scrot -u")
 ([ -d $storage_directory ]\
 	||mkdir -pv $storage_directory) \
-	; $imgmgck_cmd "$storage_directory/$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i).png"
+	; $scrot_command "$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i).png" -e 'mv $f ~/pictures/screenshots'
