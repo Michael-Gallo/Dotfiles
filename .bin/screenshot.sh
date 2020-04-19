c5f2888896c4be244b786da041d0cf4ada8f0093
Michael Gallo <michael.a.gallo95@gmail.com>
2020-04-19 07:32:19 -0400

    screenshot.sh was too clunky

--- .bin/screenshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# a simple shell script to take pictures with scrot , takes current time as default image name and saves to ~/pictures/screenshots
-# if there is an argument it'll take a picture of the entire desktop
-# filename=$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i)
-storage_directory="$HOME/pictures/screenshots"
-scrot_command=$([ -z "$1" ] && echo -e "scrot -s" || echo -e "scrot -u")
-([ -d $storage_directory ]\
-	||mkdir -pv $storage_directory) \
-	; $scrot_command "$(echo -e $( date +"%h-%d-%y-%H:%M")|dmenu -i).png" -e 'mv $f ~/pictures/screenshots'
