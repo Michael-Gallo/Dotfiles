a2441b9cd274360d7493d3c8992610f0c776ebc8
Michael Gallo <mike-fedora@localhost.localdomain>
2020-06-05 18:50:20 -0400

    smplayer no longer breaks Cinnamon

--- .config/devilspie2/cleargarbage.lua
+++ .config/devilspie2/cleargarbage.lua
@@ -1,1 +1,5 @@
-undecorate_window()
+-- remove the top bar from new windows except for smplayer (undecorating smplayer makes Cinnamon crash
+debug_print("Window Name: "..	get_window_name());
+if (get_window_name()~="SMPlayer") then
+        undecorate_window();
+end
