b24ce90da9fd4d5308c54697218537f635bcc8e4
Michael A Gallo <michael.a.gallo95@gmail.com>
2020-11-04 09:20:59 -0500

    Added bluetooth devices to floating rules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -457,7 +457,7 @@ awful.rules.rules = {
         -- Sizing
     { rule = { class = "Gimp", role = "gimp-image-window" },
           properties = { maximized = true } },
-    { rule_any = { name = {"Calculator", "calendar","Bitwarden - Mozilla Firefox","Bitwarden"}},
+    { rule_any = { name = {"Calculator", "calendar","Bitwarden - Mozilla Firefox","Bitwarden","Bluetooth Devices"}},
           properties = { floating = true } },
 }
 -- }}}
