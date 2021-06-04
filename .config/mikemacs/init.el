26153ffd8be6a190d2af6862a4f126de61d3ad2f
Michael A Gallo <michael.a.gallo95@gmail.com>
2021-06-04 19:42:55 -0400

    Created config

--- .config/mikemacs/init.el
+++ .config/mikemacs/init.el
@@ -28,9 +28,20 @@
 
 ;; Font 
 
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 150)
+
+;; You will most likely need to adjust this font size for your system!
+;; (defvar runemacs/default-font-size 100)
+
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 155)
+
+;; Set the fixed pitch face
+(set-face-attribute 'fixed-pitch nil :font "Fira Code Retina" :height 150)
+
+;; Set the variable pitch face
+(set-face-attribute 'variable-pitch nil :font "Cantarell" :height 170 :weight 'regular)
 
 (require 'use-package)
+
 (setq use-package-always-ensure t)
 
 (use-package doom-themes
@@ -38,6 +49,7 @@
 
 ;; Make ESC quit prompts
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+;; (global-set-key (kbd "<escape>") 'evil-force-normal-state)
 
 
 ;; Line Numbers
@@ -52,6 +64,8 @@
                 eshell-mode-hook))
 	      (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+(dolist (mode '(magit-mode-hook))
+  (add-hook mode (lambda () (global-set-key (kbd "<escape>") 'evil-force-normal-state))))
 				      
 ;;Logging Package
 (use-package command-log-mode)
@@ -123,19 +137,6 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-(use-package general
-  :config
-  (general-create-definer rune/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (rune/leader-keys
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")
-    "c"  '(:ignore c :which-key "comments")
-    "cc" '(comment-or-uncomment-region-or-line :which-key "toggle comment")
-    ))
 
 
 (use-package evil
@@ -148,7 +149,6 @@
   (evil-mode 1)
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
   ;; Use visual line motions even outside of visual-line-mode buffers
   (evil-global-set-key 'motion "j" 'evil-next-visual-line)
   (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
@@ -161,12 +161,7 @@
   :config
   (evil-collection-init))
 
-
 ;; Comments
-(map!
- :leader
- :prefix ("c")
- :desc "toggle comment"        "c" #'comment-or-uncomment-region-or-line)
 (defun comment-or-uncomment-region-or-line ()
     "Comments or uncomments the region or the current line if there's no active region."
     (interactive)
@@ -176,3 +171,205 @@
             (setq beg (line-beginning-position) end (line-end-position)))
         (comment-or-uncomment-region beg end)
         ))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(defhydra hydra-resize (:timeout 4)
+  "scale text"
+  ("+" evil-window-increase-height "increase height")
+  ("-" evil-window-decrease-height "decrease height")
+  ("<" evil-window-decrease-width "decrease width")
+  (">" evil-window-increase-width "increase width")
+  ("=" balance-windows "equalize")
+  ("f" nil "finished" :exit t))
+
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "/home/mike/work/JDS/Work")
+    (setq projectile-project-search-path '("/home/mike/work/JDS/Work")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)) 
+(require 'tramp)
+(add-to-list 'tramp-methods
+       '("yadm"
+       (tramp-login-program "yadm")
+       (tramp-login-args (("enter")))
+       (tramp-login-env (("SHELL") ("/bin/sh")))
+       (tramp-remote-shell "/bin/sh")
+       (tramp-remote-shell-args ("-c"))))
+
+(defun yadm--files ()
+  (let ((default-directory "~/"))
+    (cl-delete-if-not
+     #'file-exists-p
+     (process-lines "yadm" "ls-tree" "--full-tree" "-r" "--name-only" "HEAD"))))
+
+(defun yadm-find-file ()
+  (interactive)
+  (let ((default-directory  "~/"))
+    (find-file
+     (completing-read "Yadm file: " (yadm--files)))))
+
+(defun yadm-dired ()
+  (interactive)
+  (let ((default-directory "~/"))
+    (with-current-buffer (dired `("*yadm*" ,@(yadm--files)))
+      (setq-local revert-buffer-function
+                  (lambda (&rest args)
+                    (setq dired-directory
+                          (cons (car dired-directory)
+                                (yadm--files)))
+                    (apply #'dired-revert args))))))
+
+(define-minor-mode yadm-minor-mode
+  "A minor mode for magit yadm buffers."
+  :keymap (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-x d") 'yadm-dired)
+            (define-key map (kbd "C-x C-f") 'yadm-find-file)
+            (define-key map "s" 'yadm-stage)
+            map))
+
+(defun yadm-status ()
+  (interactive)
+  ; (require 'tramp)
+  (with-current-buffer (magit-status "/yadm::")
+    (yadm-minor-mode 1)))
+
+; (with-eval-after-load 'tramp
+ ; (add-to-list 'tramp-methods
+                ; '("yadm"
+                  ; (tramp-login-program "yadm")
+                  ; (tramp-login-args (("enter")))
+                  ; (tramp-remote-shell "/bin/bash")
+                  ; (tramp-remote-shell-args ("-c"))))
+  (defun yadm-stage ()
+    (interactive)
+    (let ((file
+           (let ((default-directory "~/"))
+             (read-file-name "Stage file: "))))
+      (if (equal (expand-file-name file)
+                 (expand-file-name "~/.yadm/"))
+          (user-error "Can't stage yadm dir itself.")
+        (magit-with-toplevel
+          (magit-stage-1 nil (list file))))))
+
+
+
+
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme")
+
+    "c"  '(:ignore c :which-key "comments")
+    "cc" '(comment-or-uncomment-region-or-line :which-key "toggle comment")
+
+    "w"  '(:ignore w :which-key "windows")
+    "wv" '(evil-window-vsplit :which-key "split vertical")
+    "ws" '(evil-window-split :which-key "split horizontal")
+    "wh" '(evil-window-left :which-key "move left")
+    "wl" '(evil-window-right :which-key "move right")
+    "wj" '(evil-window-down :which-key "move down")
+    "wk" '(evil-window-up :which-key "move up")
+    "wc" '(evil-window-delete :which-key "close window")
+
+    "g" '(:ignore g :which-key "magit")
+    "gg" '(magit-status :which-key "magit status")
+    "gy" '(yadm-status :which-key "yadm status")
+
+    "." '(dired :which-key "dired")
+    ))
+
+(rune/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text")
+  "tr" '(hydra-resize/body :which-key "resize windows"))
+
+
+;; ORG MODE
+
+(defun efs/org-mode-setup ()
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1))
+
+(defun efs/org-font-setup ()
+  ;; Replace list hyphen with dot
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+
+  ;; Set faces for heading levels
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
+
+(use-package org
+  :hook (org-mode . efs/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾")
+
+  (setq org-agenda-start-with-log-mode t)
+  ; When a task is set to completed, show the time it was completed
+  (setq org-log-done 'time)
+  ; Show org log as a collapsible drawer
+  (setq org-log-into-drawer t)
+  
+  (setq org-agenda-files
+	'("~/.config/mikemacs/OrgFiles/Tasks.org"))
+  (efs/org-font-setup))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+;; (defun efs/org-mode-visual-fill ()
+;;   (setq visual-fill-column-width 100
+;;         visual-fill-column-center-text t)
+;;   (visual-fill-column-mode 1))
+
+;; (use-package visual-fill-column
+;;   :hook (org-mode . efs/org-mode-visual-fill))
