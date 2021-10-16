a38d1bd5bfa7cd818135714779abd98875c734b5
michael-gallo-cc <michael.gallo@corecompete.com>
2021-10-15 20:20:43 -0400

    now applies to all razer mice

--- .bin/autoscroll_enabled.sh
+++ .bin/autoscroll_enabled.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-	for id in `xinput --list|grep 'Logitech Gaming Mouse G600\|Razer Razer DeathAdder Elite'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
+	for id in `xinput --list|grep 'Logitech Gaming Mouse G600\|Razer'|perl -ne 'while (m/id=(\d+)/g){print "$1\n";}'`; do
 		{
 		xinput --set-prop $id 'Evdev Wheel Emulation' 1
 		xinput --set-prop $id 'Evdev Wheel Emulation Button' "2"
