82cdda0f8b15d6cff92229ce333afd2ec383b095
michael-gallo-cc <michael.gallo@corecompete.com>
2021-10-15 20:21:12 -0400

    have an explanation for how to make into cronjob

--- .bin/update-artix.sh
+++ .bin/update-artix.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 # Updates the arch mirrors on artix
+# cp into /etc/cron.weekly to have run automatically
 sudo reflector --country US --sort rate --latest 50 --save /etc/pacman.d/mirrorlist-arch
