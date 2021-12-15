25d653cbab40c56489b57a2b1d967871da01a5e3
Michael A Gallo <michaelgallo@protonmail.com>
2021-12-15 06:44:48 -0500

    Added mining script

---
+++ .bin/mine.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+MINING_POOL="us1.ethermine.org"
+PORT="4444"
+CONFIG="A384"
+# Needs $ETH_ADDRESS variable to be set 
+
+teamredminer -a ethash -o stratum+tcp://$MINING_POOL:$PORT  -u $ETH_ADDRESS --eth_config=$CONFIG
