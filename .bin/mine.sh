c3b508b8c6de8bb35702f9d33d9bc4481f2e6dee
Michael A Gallo <michaelgallo@protonmail.com>
2022-01-15 21:26:37 -0500

    Mine now runs amdgpu-clocks automatically

--- .bin/mine.sh
+++ .bin/mine.sh
@@ -5,4 +5,5 @@ PORT="4444"
 CONFIG="A384"
 # Needs $ETH_ADDRESS variable to be set 
 
+sudo amdgpu-clocks
 teamredminer -a ethash -o stratum+tcp://$MINING_POOL:$PORT  -u $ETH_ADDRESS --eth_config=$CONFIG
