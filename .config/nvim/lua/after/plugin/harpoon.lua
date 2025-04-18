9dcce85923b9ca4ce00b9aa94ffd76dfcbeb6bdf
Michael Gallo <michaelgallo@protonmail.com>
2025-04-17 21:31:23 -0400

    better whichkey

--- .config/nvim/lua/after/plugin/harpoon.lua
+++ .config/nvim/lua/after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
-vim.keymap.set('n', "<leader>a", mark.add_file, {desc="add file to Harpoon"})
-vim.keymap.set('n', "<C-e>", ui.toggle_quick_menu, {desc="toggle Harpoon Quick Menu"})
+vim.keymap.set('n', "<leader>ha", mark.add_file, {desc="add file to Harpoon"})
+vim.keymap.set('n', "<leader>hh", ui.toggle_quick_menu, {desc="toggle Harpoon Quick Menu"})
 vim.keymap.set('n', "<C-h>", function() ui.nav_file(1) end, {desc = "navigate to Harpoon file 1"})
 vim.keymap.set('n', "<C-t>", function() ui.nav_file(2) end, {desc = "navigate to Harpoon file 1"})
 
--- .config/nvim/lua/after/plugin/whichkey.lua
+++ .config/nvim/lua/after/plugin/whichkey.lua
@@ -2,6 +2,7 @@ local wk = require("which-key")
 
 wk.add({
         { "<leader>p", group="file operations"},
-        { "<leader>g", group="git"}
+        { "<leader>g", group="git"},
+        { "<leader>h", group="harpoon", icon= {icon="h"}}
     }
 )
