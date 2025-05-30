e82a126981da49a731a18e92d3b8b6744b5c375b
Michael Gallo <michaelgallo@protonmail.com>
2025-05-29 23:03:15 -0400

    Actually remove copilot

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,6 +3,7 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  ---@type Snacks.Config
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
@@ -61,8 +62,6 @@ return {
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
         Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
-        Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map("<leader>uc")
         Snacks.toggle.treesitter():map("<leader>uT")
         Snacks.toggle.inlay_hints():map("<leader>uh")
         Snacks.toggle.indent():map("<leader>ug")
@@ -99,5 +98,4 @@ return {
 	"williamboman/mason-lspconfig.nvim",
     { 'echasnovski/mini.nvim', version = false },
     "nvim-tree/nvim-web-devicons",
-    "github/copilot.vim",
 }
