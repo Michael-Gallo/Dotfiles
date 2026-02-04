98bf451bf1cd13fc820f7830cc6f9e64c3ae7b8d
Michael Gallo <michaelgallo@protonmail.com>
2026-02-03 21:22:44 -0500

    fix newline commenting

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,9 +1,23 @@
 -- remove stupid autocommenting behavior
+-- Set the global default
+
+vim.opt.formatoptions:remove "c"
+vim.opt.formatoptions:remove "r"
+vim.opt.formatoptions:remove "o"
+
+-- Ensure ftplugins don't re-enable it per-buffer
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("NoCommentContinue", { clear = true }),
+  desc = "Disable comment continuation (fo-=cro)",
+  callback = function()
+    vim.opt_local.formatoptions:remove "c"
+    vim.opt_local.formatoptions:remove "r"
+    vim.opt_local.formatoptions:remove "o"
+  end,
+})
+
 return {
   opt = {
-    formatoptions = vim.opt.formatoptions:get()
-      :gsub("c", "")
-      :gsub("r", "")
-      :gsub("o", "")
+    formatoptions = vim.opt.formatoptions:get():gsub("c", ""):gsub("r", ""):gsub("o", ""),
   },
 }
