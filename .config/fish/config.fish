e2c06dda98289049f01153a2b132767b36bb2186
Michael Gallo <michaelgallo@protonmail.com>
2026-02-20 08:04:24 -0500

    setup plugins with fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g fish_greeting
+
 function fish_prompt
     set_color 28a745
     printf '%s' $USER
@@ -15,3 +17,21 @@ function fish_prompt
     set_color normal
     printf '$ '
 end
+# Warn if fisher is missing
+if not functions -q fisher
+    echo "fisher not installed. Run: curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+end
+
+function update-fish-plugins
+    fisher update
+end
+# configure fzf binds
+fzf_configure_bindings \
+    --history=\cr \
+    --directory=\ct \
+    --git_log=\cl \
+    --git_status=\cs \
+    --variables=\cv \
+    --processes=\cp
+
+
